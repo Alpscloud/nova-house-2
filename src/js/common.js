c6d201a8419a82d8ac84c9d17e981d9b2198c6e5
alpscloud <winde100kg@gmail.com>
2022-02-11 14:13:20 +0200

    added plans, investor and offer sections

--- src/js/common.js
+++ src/js/common.js
@@ -18,4 +18,24 @@ $(document).ready(function() {
 	});
 
 
+
+	$('[data-fancybox]').fancybox({
+		loop: true,
+		buttons: [
+			"zoom",
+			"close"
+		]
+	});
+
+
+	$('.js-tab-content').not(":first").hide();
+	$('.js-tab-btn:first').addClass('is-active');
+
+	$('.js-tab-btn').on('click', function(e) {
+		e.preventDefault();
+		$('.js-tab-content').removeClass('is-active');
+		$('.js-tab-btn').removeClass('is-active').eq($(this).index()).addClass('is-active');
+		$('.js-tab-content').hide().eq($(this).index()).fadeIn().addClass('is-active');
+	}).eq(0).addClass('is-active');
+
 });
