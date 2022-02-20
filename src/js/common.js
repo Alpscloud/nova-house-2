5a5dc4a4ce9f2c9e97482649135e9929be818b15
alpscloud <winde100kg@gmail.com>
2022-02-20 12:51:27 +0200

    changed video

--- src/js/common.js
+++ src/js/common.js
@@ -165,15 +165,15 @@ $(document).ready(function() {
 	
 
 
-	$('.js-tab-content').not(":first").hide();
-	$('.js-tab-btn:first').addClass('is-active');
+	$('.js-tab-content').not(".is-active").hide();
+	// $('.js-tab-btn:first').addClass('is-active');
 
 	$('.js-tab-btn').on('click', function(e) {
 		e.preventDefault();
 		$('.js-tab-content').removeClass('is-active');
 		$('.js-tab-btn').removeClass('is-active').eq($(this).index()).addClass('is-active');
 		$('.js-tab-content').hide().eq($(this).index()).fadeIn().addClass('is-active');
-	}).eq(0).addClass('is-active');
+	});
 
 
 	AOS.init({
